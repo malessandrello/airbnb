a30597169b98d9043518e90087d3e61c7db7e88a
Mauricio Alessandrello <mauricio.alessandrello@gmail.com>
2026-02-08 13:33:13 -0300

    updates

--- code.R
+++ code.R
@@ -26,6 +26,32 @@ neighnourhoods <- data1 %>%
 
 prices <- sapply(neighnourhoods, obtain_summary)
 
+# Calculate price median for each neighbourhood
+
+data2 <- data1 %>% 
+  group_by(neighbourhood_cleansed) %>% 
+  summarise(median = median(price, na.rm = TRUE), n = n()) %>% 
+  arrange(desc(median))
+
+data2 %>% 
+  ggplot(aes(median, reorder(neighbourhood_cleansed, median)))+
+  geom_col()+
+  geom_text(aes(label = paste("n = ", n)), hjust = -0.05)
+
+data2 %>% 
+  ggplot(aes(median, reorder(neighbourhood_cleansed, median)))+
+  geom_point()+
+  geom_segment(aes(x = 0, xend = median, y = reorder(neighbourhood_cleansed, median), yend = reorder(neighbourhood_cleansed, median)))+
+  geom_text(aes(label = paste("n = ", n)), hjust = -0.1)
+
+
+data1 %>% 
+  group_by(neighbourhood_cleansed) %>% 
+  mutate(median = median(price, na.rm = TRUE)) %>% 
+  ggplot(aes(price, reorder(neighbourhood_cleansed, median)))+
+  scale_x_continuous(trans = "log10")+
+  geom_boxplot()
+
 
 price_dist <- data1 %>% 
   ggplot(aes(price))+
